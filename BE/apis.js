7f486264d1efd71c178fd9c15969c165d38637e5
Udi Elenberg <udie@banshee.4g4g.com>
2017-08-20 11:51:25 +0300

    first commit

---
+++ BE/apis.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const taskModel = require('./taskModel.js')
+
+
+router.get('/api/uid/:uid/tasks', (res, req) => {
+    
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+let apis = require('./BE/apis');
+
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(express.static('app'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/', apis)
+
+app.listen(port);
+
+console.log('server runs on port', port );
+
