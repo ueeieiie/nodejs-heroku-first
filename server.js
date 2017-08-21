4a0daf4bb8b210036f71c415ba35e2f8a854bf4e
Udi Elenberg <udie@banshee.4g4g.com>
2017-08-21 10:43:05 +0300

    updated post requests

--- server.js
+++ server.js
@@ -6,24 +6,26 @@ const port = process.env.port || 3000;
 
 // Run Express app
 const app = express();
-
+const urlencodedParser = bodyParser.urlencoded({extended: true});
+const jsonParser = bodyParser.json();
 //Body-parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 //Static folder
 app.use(express.static('app'));
 
+app.use(urlencodedParser);
 // Print logs
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 
 // Routes
-app.post('/test', (req, res)=>{
-    console.log("*******************************************************");
-    res.send(req.query);
-    console.log(req.body);
-    res.end();
-});
+app.use('/', apis);
+
+// app.post('/test', (req, res)=>{
+//     console.log("*******************************************************");
+//     res.send(req.body);
+//     console.log(req.body);
+//     res.end();
+// });
 
 
 // Start listening to requests
@@ -31,5 +33,4 @@ app.listen(port);
 console.log('server runs on port', port );
 
 
-// app.use('/', apis);
 
